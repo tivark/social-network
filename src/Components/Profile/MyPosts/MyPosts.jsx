f70e97333391ee7f0d6d1c08fdef3f8cc3d56689
Vitali Krasutski <commmond@gmail.com>
2020-03-17 23:00:50 +0300

    added props in posts

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -7,8 +7,8 @@ const MyPosts = () => {
             <div className={classes.allPosts}>My posts
                 <div className={classes.newPost}>New post</div>
                 <div className={classes.posts}>
-                    <Post />
-                    <Post />
+                    <Post message='Hi, how are you?' likeCounts='15'/>
+                    <Post message="It's my first post" likeCounts='16'/>
                 </div>
             </div>
     );
--- src/Components/Profile/MyPosts/Post/Post.jsx
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import classes from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
-        <div className={classes.item}>Post 1</div>
+        <div className={classes.item}>
+            <img className={classes.image} src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt=""/>
+            {props.message}
+            <div>
+                <span>Like {props.likeCounts}</span>
+            </div>
+        </div>
     );
 }
 
