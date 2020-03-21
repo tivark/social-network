953dc94fb507062088ae2f763ade1009ea88d733
Vitali Krasutski <commmond@gmail.com>
2020-03-21 15:21:17 +0300

    change some colors

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import classes from './Header.module.css';
 const Header = (props) => {
     return (
         <header className={classes.header}>
-            <img src="https://image.winudf.com/v2/image1/Y29tLmhvbGx5cy5haWFrYXJ0X3NjcmVlbl8wXzE1NjEyNjY3MDhfMDE4/screen-0.jpg?fakeurl=1&type=.jpg" alt=""/>
+            <img src="https://pngimg.com/uploads/pacman/pacman_PNG79.png" alt=""/>
         </header>
     );
 }
