84178e17edcc7cc19f0443b1df2e95df85875d25
Vitali Krasutski <commmond@gmail.com>
2020-03-23 00:19:16 +0300

    fix addPost

--- src/Components/Profile/MyPosts/NewPost/NewPost.jsx
+++ src/Components/Profile/MyPosts/NewPost/NewPost.jsx
@@ -4,10 +4,17 @@ import classes from "./NewPost.module.css";
 const NewPost = (props) => {
 
     let newPostContent = React.createRef();
+    let addNewPost = () =>{
+        if (newPostContent.current.value.length > 3) {
+            let newText = newPostContent.current.value;
+            newPostContent.current.value = '';
+            props.addPost(newText);
+        }
+    }
     return (
         <div className={classes.box}>
             <textarea ref={newPostContent} className={classes.inputTextPost} rows="4"></textarea>
-            <button type="button" value="Add post" className={classes.addPostButton} onClick={props.addPost}>Add post</button>
+            <button value="Add post" className={classes.addPostButton} onClick={addNewPost}>Add post</button>
         </div>
     )
 };
