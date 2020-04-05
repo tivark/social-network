69b457fb16df469b7912bc0948bf5daa12ab038c
Vitali Krasutski <commmond@gmail.com>
2020-04-05 19:11:50 +0300

    fix class

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,35 +1,47 @@
 import React from "react";
 import classes from "./Users.module.css";
+import userPhoto from "../../assets/images/user.jpg";
 import * as axios from "axios";
-import userPhoto from "../../assets/images/user.jpg"
 
-const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items)
-            })
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        this.getUsers();
     }
-    return (
-        <div className={classes.box}>
-            {
-                props.users.map(el => <div className={classes.userWrapper} key={el.id}>
-                    <div className={classes.avatarWrapper}><img src={el.photos.small != null ? el.photos.small : userPhoto} alt={el.name}
-                                                                className={classes.avatarImage}/></div>
-                    <div className={classes.userDetails}>
-                        <div>{el.name}</div>
-                        <div>{el.status}</div>
-                        <div className={classes.followButtonWrapper}>
-                            <button className={classes.followButton} onClick={() => {
-                                props.followSwitcher(el.id)
-                            }}>
-                                {el.followed ? 'Unfollow' : 'Follow'}
-                            </button>
+
+    getUsers = () => {
+        if (this.props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    this.props.setUsers(response.data.items)
+                })
+        }
+    }
+
+    render() {
+        return (
+            <div className={classes.box}>
+                {
+                    this.props.users.map(el => <div className={classes.userWrapper} key={el.id}>
+                        <div className={classes.avatarWrapper}><img
+                            src={el.photos.small != null ? el.photos.small : userPhoto} alt={el.name}
+                            className={classes.avatarImage}/></div>
+                        <div className={classes.userDetails}>
+                            <div>{el.name}</div>
+                            <div>{el.status}</div>
+                            <div className={classes.followButtonWrapper}>
+                                <button className={classes.followButton} onClick={() => {
+                                    this.props.followSwitcher(el.id)
+                                }}>
+                                    {el.followed ? 'Unfollow' : 'Follow'}
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                </div>)
-            }
-        </div>
-    );
-};
+                    </div>)
+                }
+            </div>
+        )
+    }
+}
+
 export default Users;
